ab7cf2f724c8e4b350415b584efa2819cf1ec65d
Andrei <andrei.cioboata@tquila-automation.com>
2022-10-01 06:07:56 +0200

    added assignments 1 and 2

--- Basics_Numpy.py
+++ Basics_Numpy.py
@@ -1,5 +1,7 @@
 import math
+from turtle import delay
 import numpy as np
+import time
 def basic_sigmoid(x):
     s=1/(1+math.exp(-x))
     return s
@@ -16,6 +18,41 @@ def sigmoid_derivative(x):
 def image2vector(image):
     v= image.reshape(image.shape[0]*image.shape[1]*image.shape[2],1)
     return v
+
+def normalize_rows(x):
+    x_norm = np.linalg.norm(x, axis=1, keepdims=True)
+    x = x/x_norm
+    return x
+
+def softmax(x):
+    x_exp = np.exp(x)
+    x_sum = np.sum(x_exp, axis = 1, keepdims = True)
+    s = x_exp/x_sum
+    return s
+
+def L1(yhat, y):
+    """
+    Arguments:
+    yhat -- vector of size m (predicted labels)
+    y -- vector of size m (true labels)
+    
+    Returns:
+    loss -- the value of the L1 loss function defined above
+    """
+    loss = np.sum(np.abs(yhat-y))
+    return loss
+
+def L2(yhat, y):
+    """
+    Arguments:
+    yhat -- vector of size m (predicted labels)
+    y -- vector of size m (true labels)
+    
+    Returns:
+    loss -- the value of the L2 loss function defined above
+    """
+    loss = np.dot(yhat,yhat)+np.dot(y,y)-2*np.sum(np.multiply(yhat,y))
+    return loss
 
 print("basic_sigmoid(1) = " + str(basic_sigmoid(1)))
 
@@ -37,3 +74,87 @@ t_image = np.array([[[ 0.67826139,  0.29380381],
                     [ 0.34144279,  0.94630077]]])
 
 print ("image2vector(image) = " + str(image2vector(t_image)))
+x = np.array([[0, 3, 4],
+              [1, 6, 4]])
+print("normalizeRows(x) = " + str(normalize_rows(x)))
+t_x = np.array([[9, 2, 5, 0, 0],
+                [7, 5, 0, 0 ,0]])
+print("softmax(x) = " + str(softmax(t_x)))
+
+x1 = [9, 2, 5, 0, 0, 7, 5, 0, 0, 0, 9, 2, 5, 0, 0]
+x2 = [9, 2, 2, 9, 0, 9, 2, 5, 0, 0, 9, 2, 5, 0, 0]
+
+### CLASSIC DOT PRODUCT OF VECTORS IMPLEMENTATION ###
+tic = time.process_time()
+dot = 0
+
+for i in range(len(x1)):
+    dot += x1[i] * x2[i]
+toc = time.process_time()
+print ("dot = " + str(dot) + "\n ----- Computation time = " + str(1000 * (toc - tic)) + "ms")
+
+### CLASSIC OUTER PRODUCT IMPLEMENTATION ###
+tic = time.process_time()
+outer = np.zeros((len(x1), len(x2))) # we create a len(x1)*len(x2) matrix with only zeros
+
+for i in range(len(x1)):
+    for j in range(len(x2)):
+        outer[i,j] = x1[i] * x2[j]
+toc = time.process_time()
+print ("outer = " + str(outer) + "\n ----- Computation time = " + str(1000 * (toc - tic)) + "ms")
+
+### CLASSIC ELEMENTWISE IMPLEMENTATION ###
+tic = time.process_time()
+mul = np.zeros(len(x1))
+for i in range(len(x1)):
+    mul[i] = x1[i] * x2[i]
+toc = time.process_time()
+
+print ("elementwise multiplication = " + str(mul) + "\n ----- Computation time = " + str(1000 * (toc - tic)) + "ms")
+
+### CLASSIC GENERAL DOT PRODUCT IMPLEMENTATION ###
+W = np.random.rand(3,len(x1)) # Random 3*len(x1) numpy array
+tic = time.process_time()
+gdot = np.zeros(W.shape[0])
+
+for i in range(W.shape[0]):
+    for j in range(len(x1)):
+        gdot[i] += W[i,j] * x1[j]
+toc = time.process_time()
+print ("gdot = " + str(gdot) + "\n ----- Computation time = " + str(1000 * (toc - tic)) + "ms")
+
+x1 = [9, 2, 5, 0, 0, 7, 5, 0, 0, 0, 9, 2, 5, 0, 0]
+x2 = [9, 2, 2, 9, 0, 9, 2, 5, 0, 0, 9, 2, 5, 0, 0]
+
+### VECTORIZED DOT PRODUCT OF VECTORS ###
+tic = time.process_time()
+dot = np.dot(x1,x2)
+toc = time.process_time()
+print ("dot = " + str(dot) + "\n ----- Computation time = " + str(1000 * (toc - tic)) + "ms")
+
+### VECTORIZED OUTER PRODUCT ###
+tic = time.process_time()
+outer = np.outer(x1,x2)
+toc = time.process_time()
+print ("outer = " + str(outer) + "\n ----- Computation time = " + str(1000 * (toc - tic)) + "ms")
+
+### VECTORIZED ELEMENTWISE MULTIPLICATION ###
+tic = time.process_time()
+mul = np.multiply(x1,x2)
+toc = time.process_time()
+print ("elementwise multiplication = " + str(mul) + "\n ----- Computation time = " + str(1000*(toc - tic)) + "ms")
+
+### VECTORIZED GENERAL DOT PRODUCT ###
+tic = time.process_time()
+dot = np.dot(W,x1)
+toc = time.process_time()
+print ("gdot = " + str(dot) + "\n ----- Computation time = " + str(1000 * (toc - tic)) + "ms")
+
+yhat = np.array([.9, 0.2, 0.1, .4, .9])
+y = np.array([1, 0, 0, 1, 1])
+print("L1 = " + str(L1(yhat, y)))
+
+yhat = np.array([.9, 0.2, 0.1, .4, .9])
+y = np.array([1, 0, 0, 1, 1])
+
+print("L2 = " + str(L2(yhat, y)))
--- hello.py
+++ hello.py
@@ -1,2 +1,2 @@
-msg = "Hello World4"
+msg = "Hello World"
 print(msg)
